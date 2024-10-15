9457aca60b5394769870122e5b632f90b1d0709f
akkayazs <zs_akkaya@outlook.com>
2024-10-15 23:33:28 +0300

    Homepage updated

--- frontend/real-time-chat-app/src/App.js
+++ frontend/real-time-chat-app/src/App.js
@@ -1,7 +1,38 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div>
+      <div class="text-center m-10">
+        <h1 class="text-4xl font-bold">
+          Welcome to Real Time Chat Application
+        </h1>
+        <p class="text-sm italic">
+          Enter the room name you want to join and start talking with people!
+        </p>
+      </div>
+
+      <fieldset class="flex flex-col items-center gap-2">
+        <label for="room" class="italic">
+          Room Name
+        </label>
+        <input
+          id="room"
+          type="text"
+          placeholder="Enter the room name here"
+          class="rounded border border-gray-800 px-10 py-2"
+        ></input>
+        <label for="name" class="mt-6 italic">
+          User Name
+        </label>
+        <input
+          id="name"
+          type="text"
+          placeholder="Enter your user name here"
+          class="rounded border border-gray-800 px-10 py-2"
+        ></input>
+        <button class="bg-gray-600 text-gray-200 px-16 py-2 rounded hover:bg-gray-800 hover:text-white mt-6">
+          Join
+        </button>
+      </fieldset>
+    </div>
+  );
 }
